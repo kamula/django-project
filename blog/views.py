1dce2479019ea1b9a5317388ec61fddf27f561aa
kamula <kamulaisaac@gmail.com>
2020-02-24 18:06:41 +0300

    correction

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-
-post = [
-    {
-        'author': 'isaac',
-        'title': 'blog 1',
-        "content": "first blog content",
-        'date': '23 January 2020'
-    },
-    {
-        'author': 'Florence',
-        'title': 'blog 2',
-        "content": "second blog content",
-        'date': '24 January 2020'
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': post
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
