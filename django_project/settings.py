61cd39f190fefb5db72e3b22a616c3b654f71dcb
kamula <kamulaisaac@gmail.com>
2020-02-28 10:18:14 +0300

    profile

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = ''
+
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
